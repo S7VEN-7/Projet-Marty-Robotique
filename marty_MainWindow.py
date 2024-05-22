46eea259aeaf20312af9185e0ac589993825484b
mateohubert2 <mathub2003@gmail.com>
2024-05-22 15:23:13 +0200

    Utilisation des touches du clavier

--- marty_MainWindow.py
+++ marty_MainWindow.py
@@ -2,12 +2,18 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QWidget
 from PyQt6.QtCore import pyqtSlot
+from PyQt6.QtGui import QKeyEvent
+from martypy import Marty
 from ui_MainWindow import Ui_MainWindow
 class MartyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MartyMainWindow,self).__init__(parent)
         self.setupUi(self)
-        
+        self.myMarty = Marty("wifi", "192.168.0.100")
+        self.up.clicked.connect(self.avancer)
+        self.down.clicked.connect(self.reculer)
+        self.right.clicked.connect(self.droite)
+        self.left.clicked.connect(self.gauche)
     @pyqtSlot()
     def on_actionQuitter_triggered(self):
         self.close()
@@ -20,3 +26,27 @@ class MartyMainWindow(QMainWindow, Ui_MainWindow):
             event.accept()
         else:
             event.ignore()
+    def avancer(self):
+        self.myMarty.walk(2, 'auto', 0)
+    def reculer(self):
+        self.myMarty.walk(2, 'auto', 0, -25)
+    def droite(self):
+        self.myMarty.sidestep("right", 2)
+    def gauche(self):
+        self.myMarty.sidestep("left", 2)
+    def demi_droite(self):
+        self.myMarty.walk(2, 'auto', 40, 30, 3000)
+        self.myMarty.walk(2, 'auto', 40, 30, 3000)
+    def keyPressEvent(self, event: QKeyEvent):
+        key = event.key()
+        if key == Qt.Key.Key_Z:
+            self.avancer()
+        elif key == Qt.Key.Key_Q:
+            self.gauche()
+        elif key == Qt.Key.Key_S:
+            self.reculer()
+        elif key == Qt.Key.Key_D:
+            self.droite()
+        elif key == Qt.Key.Key_C:
+            self.demi_droite()
+    
