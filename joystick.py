498eee22c4a3e0007133f3050e61bfe7a615d21a
S7VEN-7 <thomevince@gamail.com>
2024-05-27 15:00:33 +0200

    Faire bouger Marty avec la manette réussi

--- joystick.py
+++ joystick.py
@@ -1,177 +1,143 @@
+import sys
 import pygame
-import json, os
-import connexion
+import json
+import os
+from martypy import Marty
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget
+from PyQt6.QtCore import QTimer
 
-class JoystickController ():
+class JoystickController(QMainWindow):
     def __init__(self):
+        super().__init__()
+
         pygame.init()
         self.running = True
-        self.LEFT, self.RIGHT, self.UP, self.DOWN  = False, False, False, False
-        self.clock = pygame.time.Clock()
+        self.LEFT, self.RIGHT, self.UP, self.DOWN = False, False, False, False
 
-        # Initialize controller
+        self.initUI()
+        self.initJoystick()
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.game_loop)
+        self.timer.start(16)  # 60 FPS
+
+        self.myMarty = Marty("wifi", "192.168.0.104")
+
+    def initUI(self):
+        self.setWindowTitle("Joystick Controller")
+        self.setGeometry(100, 100, 400, 300)
+
+        self.text_edit = QTextEdit()
+        self.text_edit.setReadOnly(True)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.text_edit)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+    def initJoystick(self):
+        if pygame.joystick.get_count() == 0:
+            print("No joystick connected. Please connect a joystick and restart the application.")
+            self.running = False
+            return
+        
         self.joystick = pygame.joystick.Joystick(0)
         self.joystick.init()
 
+        print("Manette connectée !")
+        
         with open(os.path.join(os.path.dirname(__file__), './game_controller/ps4_keys.json')) as file:
             button_keys = json.load(file)
 
-        self.joystick_keys = []
-        for key in button_keys:
-            self.joystick_keys.append(key)
+        self.joystick_keys = [key for key in button_keys]
 
-        # 0: Left analog horizonal, 1: Left Analog Vertical, 2: Right Analog Horizontal
-        # 3: Right Analog Vertical 4: Left Trigger, 5: Right Trigger
-        self.analog_keys = {0:0, 1:0, 2:0, 3:0, 4:-1, 5: -1 }
+        self.analog_keys = {0: 0, 1: 0, 2: 0, 3: 0, 4: -1, 5: -1}
 
+    def game_loop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                self.close()
 
-    def run(self):
-        # START OF GAME LOOP
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-
-                # BUTTON PRESSES
-                self.handle_button_presses(event)
-
-                # ANALOG INPUTS
-                self.handle_analog_inputs(event)
-
+            self.handle_button_presses(event)
+            self.handle_analog_inputs(event)
 
     def handle_button_presses(self, event):
-        # BUTTON PRESSES
         if event.type == pygame.JOYBUTTONDOWN:
-            print(self.joystick_keys[event.button])
-            if self.joystick_keys[event.button] == "cross":
+            button_name = self.joystick_keys[event.button]
+            self.log_message(button_name)
+            if button_name == "down_arrow":
                 self.joystick_rumble(0.65, 1, 250)
-                connexion.marty.celebrate() # MARTY
-            elif self.joystick_keys[event.button] == "circle":
+                self.reculer()
+            elif button_name == "right_arrow":
                 self.joystick_rumble(0, 1, 250)
-            elif self.joystick_keys[event.button] == "triangle":
+                self.droite()
+            elif button_name == "up_arrow":
                 self.joystick_rumble(0.65, 1, 250)
-            elif self.joystick_keys[event.button] == "square":
+                self.avancer()
+            elif button_name == "left_arrow":
                 self.joystick_rumble(0.65, 0, 250)
-            elif self.joystick_keys[event.button] == "share":
+                self.gauche()
+            elif button_name == "circle":
+                self.joystick_rumble(0.65, 1, 250)
+                self.demi_droite()
+            elif button_name == "square":
+                self.joystick_rumble(0.65, 1, 250)
+                self.demi_gauche()
+            elif button_name == "share":
                 self.running = False
-
+                self.close()
 
     def handle_analog_inputs(self, event):
-        # ANALOG INPUTS
         if event.type == pygame.JOYAXISMOTION:
             self.analog_keys[event.axis] = event.value
-            # L-joystick
-            # Horizontal Analog
-            if abs(self.analog_keys[0]) > .2:
-                if self.analog_keys[0] < -.2:
-                    self.LEFT = True
-                    print(self.analog_keys[0], " LEFT (L-joystick)")
-                else:
-                    self.LEFT = False
+            self.update_movement(event.axis, event.value)
 
-                if self.analog_keys[0] > .2:
-                    self.RIGHT = True
-                    print(self.analog_keys[0], " RIGHT (L-joystick)")
-                else:
-                    self.RIGHT = False
+    def update_movement(self, axis, value):
+        if axis in [0, 2]:  # Horizontal
+            direction = "LEFT" if value < -0.2 else "RIGHT" if value > 0.2 else None
+        elif axis in [1, 3]:  # Vertical
+            direction = "UP" if value < -0.2 else "DOWN" if value > 0.2 else None
+        else:
+            direction = None
 
-            # Vertical Analog
-            if abs(self.analog_keys[1]) > .2:
-                if self.analog_keys[1] < -.2:
-                    self.UP = True
-                    print(self.analog_keys[1], " UP (L-joystick)")
-                else:
-                    self.UP = False
+        if direction:
+            self.log_message(f"{value} {direction}")
 
-                if self.analog_keys[1] > .2:
-                    self.DOWN = True
-                    print(self.analog_keys[1], " DOWN (L-joystick)")
-                else:
-                    self.DOWN = False
-            
-            # R-joystick
-            # Horizontal Analog
-            if abs(self.analog_keys[2]) > .2:
-                if self.analog_keys[2] < -.2:
-                    self.LEFT = True
-                    print(self.analog_keys[2], " LEFT (R-joystick)")
-                else:
-                    self.LEFT = False
+        if axis == 4 and value == 1:
+            self.log_message("LEFT TRIGGER")
+        if axis == 5 and value == 1:
+            self.log_message("RIGHT TRIGGER")
 
-                if self.analog_keys[2] > .2:
-                    self.RIGHT = True
-                    print(self.analog_keys[2], " RIGHT (R-joystick)")
-                else:
-                    self.RIGHT = False
 
-            # Vertical Analog
-            if abs(self.analog_keys[3]) > .2:
-                if self.analog_keys[3] < -.2:
-                    self.UP = True
-                    print(self.analog_keys[3], " UP (R-joystick)")
-                else:
-                    self.UP = False
+    def avancer(self):
+        self.myMarty.walk(2, 'auto', 0)
 
-                if self.analog_keys[3] > .2:
-                    self.DOWN = True
-                    print(self.analog_keys[3], " DOWN (R-joystick)")
-                else:
-                    self.DOWN = False
+    def reculer(self):
+        self.myMarty.walk(2, 'auto', 0, -25)
 
-                # Triggers
-            if self.analog_keys[4] == 1:  # Left trigger
-                # color += 2
-                print("LEFT TRIGGER")
-            if self.analog_keys[5] == 1:  # Right Trigger
-                # color -= 2
-                print("RIGHT TRIGGER")
+    def droite(self):
+        self.myMarty.sidestep("right", 2)
 
+    def gauche(self):
+        self.myMarty.sidestep("left", 2)
+
+    def demi_droite(self):
+        self.myMarty.walk(4, 'auto', -13, 30, 2000)
+    
+    def demi_gauche(self):
+        self.myMarty.walk(4, 'auto', 13, 30, 2000)
 
     def joystick_rumble(self, low, high, duration):
-        # RUMBLE THE CONTROLLER
         self.joystick.rumble(low, high, duration)
-        
 
-############################################################################
+    def log_message(self, message):
+        self.text_edit.append(message)
 
-# Run the controller
-
-controller = JoystickController()
-controller.run()
-
-################################# LIGHTBAR #################################
-# import time
-# import hid
-# #Pour le moment ça ne fonctionne pas...
-
-# def set_ps4_lightbar_color(red, green, blue):
-#     # Find the PS4 controller
-#     devices = hid.enumerate(1356, 2508)
-#     if len(devices) == 0:
-#         print("PS4 controller not found.")
-#     return
-
-# # Open the device
-# device_info = devices[0]
-# device = hid.device()
-# device.open_path(device_info['path'])
-
-# # Prepare the HID report
-# report = [0x11, 0x80, 0x00, 0x00, red, green, blue, 0x00]
-
-# # Send the HID report to change the light bar color
-# device.write(report)
-
-# # Close the device
-# device.close()
-
-# # Example: Set light bar to red
-# set_ps4_lightbar_color(255, 0, 0)
-# time.sleep(5) # Keep it red for 5 seconds
-
-# # Example: Set light bar to blue
-# set_ps4_lightbar_color(0, 0, 255)
-# time.sleep(5)
-# set_ps4_lightbar_color(0, 255, 0)
-
-############################################################################
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    controller = JoystickController()
+    controller.show()
+    sys.exit(app.exec())
