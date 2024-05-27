80aa9e8c1ea97d4fab1cb56d1e9c6e3b207becd1
S7VEN-7 <thomevince@gamail.com>
2024-05-27 15:02:26 +0200

    Suppression d'une fichier temporaire

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import *
-from ui_MainWindow import Ui_MainWindow
-from marty_MainWindow import MartyMainWindow
-app = QApplication(sys.argv)
-window = MartyMainWindow()
-window.show()
-rc = app.exec()
-sys.exit(rc)
